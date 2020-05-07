0b83e7a38826e40246b69cd809898026381ab15e
Dvoriadkin, Nikita <dvorinik@fel.cvut.cz>
2020-05-07 13:56:20 +0200

    Cart Item Added

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,12 +1,26 @@
 import React from "react";
+import CartItem from "./cart-item/CartItem";
+import './Cart.css'
 
 function Cart() {
 
     return(
         <div>
             <h1 style={{paddingLeft: "20px", marginTop: "20px", marginBottom: "10px"}}>Cart</h1>
-            <div className="app">
+            <div className="cart-page">
+                <div className="cart">
+                    <CartItem name="Beef" price="200 Czk"/>
+                    <CartItem name="Beef" price="200 Czk"/>
+                    <CartItem name="Beef" price="200 Czk"/>
+                </div>
+
+                <div className="totals">
+                    <h3>Total: 2000 CZK</h3>
+                    <button className="submit-button">Submit</button>
+                </div>
+
             </div>
+
         </div>
     );
 }
---
+++ src/components/cart/cart-item/CartItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './CartItem.css'
+
+function CartItem({name, price, picture}) {
+    return(
+            <div className="item-placed">
+                <img src={process.env.PUBLIC_URL + "/img/meat.jpg"} style={{width: "20%", height: "80%"}}/>
+                <h5>{name}</h5>
+
+                <div className="counter-block">
+                    <button style={{width: "30px", height: "30px", borderRadius: "15px", fontSize: "22px"}}>+</button>
+                    <h4>0</h4>
+                    <button style={{width: "30px", height: "30px", borderRadius: "15px", fontSize: "22px"}}>-</button>
+                </div>
+
+                <h4>{price}</h4>
+
+                <button style={{width: "30px", height: "30px", borderStyle: 'none', backgroundColor: "#ededed", fontSize: "22px"}}>x</button>
+
+            </div>
+    );
+}
+
+export default CartItem;
